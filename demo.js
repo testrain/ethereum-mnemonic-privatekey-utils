307471c2bd36aaaaa9a8eb97977894f535f020e5
testtrain <hsyjonny@gmail.com>
2021-03-01 06:50:44 +0800

    dump keystore

--- demo.js
+++ demo.js
@@ -1,7 +1,8 @@
 const pkutils = require('./index');
+const fs = require('fs');
 
 pkutils.debug = true;
-
+/*
 const mnemonic = 'yesterday once more happy bride smile short lovers make life sound great';
 const password = 'this is a totally long password';
 
@@ -11,10 +12,14 @@ console.log('password               : %s', password);
 const privateKeyGenList = pkutils.getPrivateKeysFromMnemonic(mnemonic, 10);
 
 const keystores = pkutils.getKeystoresFromPrivateKey(privateKeyGenList, password);
+*/
 
+const keystore = JSON.parse(fs.readFileSync(process.env.KEYSTORE_PATH));
+console.log('~~~ keystore = ', keystore);
+const password = process.env.PASS
 
-// const privateKeyParsed = pkutils.getPrivateKeyFromKeystore(keystore, password);
-// console.log('pkey from keystore     : 0x%s', privateKeyParsed);
+ const privateKeyParsed = pkutils.getPrivateKeyFromKeystore(keystore, password);
+ console.log('pkey from keystore     : 0x%s', privateKeyParsed);
 
 // const account = keystore.address;
 // console.log('account address        : 0x%s', account);
